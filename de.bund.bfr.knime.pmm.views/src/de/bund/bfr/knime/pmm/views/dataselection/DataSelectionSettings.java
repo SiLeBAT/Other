5980c613dfc0969c8cceca87081a018c19c25019
cthoens <christian.thoens@bfr.bund.de>
2014-03-18 17:26:33 +0100

    unused projects removed

--- de.bund.bfr.knime.pmm.views/src/de/bund/bfr/knime/pmm/views/dataselection/DataSelectionSettings.java
+++
@@ -1,7 +0,0 @@
-package de.bund.bfr.knime.pmm.views.dataselection;
-
-import de.bund.bfr.knime.pmm.views.MultiViewerSettings;
-
-public class DataSelectionSettings extends MultiViewerSettings {
-
-}
--- de.bund.bfr.knime.pmm.views/src/de/bund/bfr/knime/pmm/views/tertiarymodelview/TertiaryModelViewSettings.java
+++
@@ -1,7 +0,0 @@
-package de.bund.bfr.knime.pmm.views.tertiarymodelview;
-
-import de.bund.bfr.knime.pmm.views.MultiModelViewerSettings;
-
-public class TertiaryModelViewSettings extends MultiModelViewerSettings {
-
-}
