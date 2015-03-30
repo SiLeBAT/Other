feec8b51ddba2ef73b1960d10e8bd7e2136fb7c7
thoens <christian.thoens@gmail.com>
2015-03-30 11:33:31 +0200

    copyrights fixed

--- de.bund.bfr.pmf/src/de/bund/bfr/pmf/sbml/PMFAnnotation.groovy
+++ de.bund.bfr.pmf/src/de/bund/bfr/pmf/sbml/PMFAnnotation.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Federal Institute for Risk Assessment (BfR), Germany
+ * Copyright (c) 2015 Federal Institute for Risk Assessment (BfR), Germany
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,12 +8,15 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Department Biological Safety - BfR
+ *******************************************************************************/
 package de.bund.bfr.pmf.sbml
 
 import groovy.transform.InheritConstructors
