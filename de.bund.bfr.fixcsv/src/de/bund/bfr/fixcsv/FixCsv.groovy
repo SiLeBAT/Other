27429e314ad8cd4ddd2646eb145df3524f5c1947
thoens <christian.thoens@bfr.bund.de>
2016-05-24 16:28:47 +0200

    FixCsv committed

---
+++ de.bund.bfr.fixcsv/src/de/bund/bfr/fixcsv/FixCsv.groovy
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Federal Institute for Risk Assessment (BfR), Germany
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Department Biological Safety - BfR
+ *******************************************************************************/
+package de.bund.bfr.fixcsv
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files
+
+class FixCsv {
+
+	static String FILE_NAME = "G:/Abteilung-4/Antibiotikagabe/vetproof/data/160201/Schwein/nicht-behandelte_Quartale_2016-05-20T11-51-27.csv"
+	static String SEPERATOR = ";"
+
+	static main(args) {
+		def oldFile = new File(FILE_NAME)
+		def newFile = new File(FILE_NAME.take(FILE_NAME.lastIndexOf('.')) + "_fixed.csv")
+		
+		newFile.write(oldFile.getText('utf-8').replaceAll(/"[^;"]+"/, 
+			{ it.replace("\"","\"\"") }).replaceAll(/(?m)^[^"]*"[^"]*$/, 
+			{ it.replace("\"","\"\"") }), 'utf-8')		
+	}
+}
