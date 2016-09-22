8c10e23886ebc841fb6ff9fecd09b98102df6f87
arminweiser <arminweiser@192.168.2.12>
2016-09-23 00:05:54 +0200

    XmlValidator updated

--- de.bund.bfr.busstop/src/de/bund/bfr/busstopp/util/XmlValidator.java
+++ de.bund.bfr.busstop/src/de/bund/bfr/busstopp/util/XmlValidator.java
@@ -1,52 +1,35 @@
 package de.bund.bfr.busstopp.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.SchemaFactory;
 
 import org.xml.sax.SAXException;
 
-import de.nrw.verbraucherschutz.idv.daten.Kontrollpunktmeldung;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
 
 public class XmlValidator {
 
-	public static void main(String[] args) throws JAXBException {
+	public static void main(String[] args) {
 		System.err.println(new XmlValidator().validate("/Users/arminweiser/Desktop/xml_test/Anleitung_pmmlab.txt"));
+		System.err.println(new XmlValidator().validate("/Users/arminweiser/Desktop/xml_test/bbk/bbk1.xml"));
 	}
 
-	@SuppressWarnings("unchecked")
 	public boolean validate(String filename) {
-		boolean result = false;
-		Unmarshaller reader;
-		try {
-			reader = JAXBContext.newInstance(Kontrollpunktmeldung.class.getPackage().getName())
-					.createUnmarshaller();
-
-			reader.setSchema(SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-					.newSchema(Kontrollpunktmeldung.class.getResource(
-							"/de/nrw/verbraucherschutz/idv/daten/main/de.nrw.verbraucherschutz.idv.dienste.2016.2.warenrueckverfolgung.transport.schema.xsd")));
-	
-			File file = new File(filename);
-			if (file.exists() && file.isFile()) {
-				if (file.getName().endsWith(".xml")) {
-					System.out.println("----- " + file.getName() + " -----");
-
-					@SuppressWarnings("unused")
-					Kontrollpunktmeldung meldung = ((JAXBElement<Kontrollpunktmeldung>) reader.unmarshal(file)).getValue();
-					
-					result = true;
-				}
-			}
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		}
-		return result;		
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      	  	URL xsd = getClass().getResource("/de/bund/bfr/busstopp/util/xsd/main/de.nrw.verbraucherschutz.idv.dienste.2016.2.warenrueckverfolgung.transport.schema.xsd");
+            Schema schema = factory.newSchema(xsd);
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(filename)));
+        } catch (IOException | SAXException e) {
+            //System.out.println("Exception: "+e.getMessage());
+            return false;
+        }
+        return true;
 	}
 }
